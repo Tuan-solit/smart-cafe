2737d182aa2f05aed561e1e230ff24a7fd2975f5
triet2203 <minhtriet2203@gmail.com>
2026-08-13 14:40:41 +0700

    feat: complete admin staff management

---
+++ src/main/java/com/module3/ccafe/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.module3.ccafe.repository;
+
+import com.module3.ccafe.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/module3/ccafe/repository/UserRepository.java
@@ -0,0 +1,42 @@
+package com.module3.ccafe.repository;
+
+import com.module3.ccafe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("""
+            select u
+            from User u
+            join u.role r
+            where r.name = :roleName
+            """)
+    List<User> findUserByRoleName(@Param("roleName") String roleName);
+
+    @Query("""
+            select u
+            from User u
+            join u.role r
+            where u.userId = :userId
+            and r.name = :roleName
+            """)
+    Optional<User> findUserByIdAndRoleName(@Param("userId") Integer userId,
+                                           @Param("roleName") String roleName);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
+
+    @Query("""
+            select count(u)
+            from User u
+            join u.role r
+            where r.name = :roleName
+            """)
+    long countUsersByRoleName(@Param("roleName") String roleName);
+}
