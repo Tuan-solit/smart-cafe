963e4c65ab889a6f82c9b74ebb801e66bf20940d
triet2203 <minhtriet2203@gmail.com>
2026-08-13 15:10:20 +0700

    update

--- src/main/java/com/module3/ccafe/repository/UserRepository.java
+++ src/main/java/com/module3/ccafe/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.module3.ccafe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
